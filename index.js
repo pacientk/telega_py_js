cf70550d7a5ae155773cb2e76dcbb168e542ed56
KirTer <kirill.ter@gmail.com>
2023-04-29 01:29:01 +0300

    ...

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const TelegramBot = require('node-telegram-bot-api');
 const child_process = require('child_process');
+const {gameOptions, againOptions} = require('./options')
 const moment = require('moment');
 require('dotenv').config();
 
@@ -7,24 +8,6 @@ const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, { polling: true });
 
 const chats = {};
 
-const gameOptions = {
-    reply_markup: JSON.stringify({
-        inline_keyboard: [
-            [{ text: '1', callback_data: '1' }, { text: '2', callback_data: '2' }, { text: '3', callback_data: '3' }],
-            [{ text: '4', callback_data: '4' }, { text: '5', callback_data: '5' }, { text: '6', callback_data: '6' }],
-            [{ text: '7', callback_data: '7' }, { text: '8', callback_data: '8' }, { text: '9', callback_data: '9' }],
-            [{ text: '10', callback_data: '10' }],
-        ],
-    }),
-};
-
-const againOptions = {
-    reply_markup: JSON.stringify({
-        inline_keyboard: [
-            [{ text: 'А ну, еще раз!', callback_data: '/again' }],
-        ],
-    }),
-};
 const startGame = async (chatId) => {
     const randomNumber = Math.floor(Math.random() * 10);
     chats[chatId] = randomNumber;
