b8a90e7c171987e78ff9a66b1797aa621bb46c52
KirTer <kirill.ter@gmail.com>
2023-05-07 20:24:05 +0300

    db

--- src/db.js
+++ src/db.js
@@ -5,6 +5,3 @@ module.exports = new Sequelize('defaultdb', 'vultradmin', 'AVNS_9I74M7C9XmwmVRx5
    host: 'vultr-prod-062f0b48-7750-4b0a-80ef-0d8382e1d86e-vultr-prod-2b1d.vultrdb.com',
    port: '16751',
 });
-// module.exports = new Sequelize(
-//    'postgresql://vultr-prod-062f0b48-7750-4b0a-80ef-0d8382e1d86e-vultr-prod-2b1d.vultrdb.com:16751/defaultdb'
-// );
